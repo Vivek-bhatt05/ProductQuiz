40352abba1d7f69c092889c09d4a60bcd1aceb7c
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-10-05 18:22:59 +0530

    basics

---
+++ product/src/utils/questions.js
@@ -0,0 +1,45 @@
+const quizQuestions = [
+    {
+      question: "What is the capital of France?",
+      options: ["London", "Berlin", "Paris", "Madrid"],
+    },
+    {
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain"],
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Earth", "Venus", "Mars", "Jupiter"],
+    },
+    {
+      question: "In which year did Christopher Columbus first reach the Americas?",
+      options: ["1492", "1776", "1865", "1620"],
+    },
+    {
+      question: "Who is known as the father of modern physics?",
+      options: ["Isaac Newton", "Albert Einstein", "Galileo Galilei", "Stephen Hawking"]
+    },
+    {
+      question: "What is the chemical symbol for water?",
+      options: ["O2", "H2O", "CO2", "NaCl"],
+    },
+    {
+      question: "What gas do plants absorb from the atmosphere during photosynthesis?",
+      options: ["Oxygen", "Carbon dioxide", "Nitrogen", "Hydrogen"],
+    },
+    {
+      question: "Which planet is known as the 'Red Planet'?",
+      options: ["Venus", "Earth", "Mars", "Jupiter"],
+    },
+    {
+      question: "What is the process by which plants make their own food using sunlight?",
+      options: ["Respiration", "Photosynthesis", "Digestion", "Fermentation"],
+    },
+    {
+      question: "Who was the first President of the United States?",
+      options: ["Benjamin Franklin", "Thomas Jefferson", "George Washington", "John Adams"],
+    }
+  ];
+  
+export default quizQuestions;
+  
