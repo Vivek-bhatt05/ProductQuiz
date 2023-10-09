de73f96840bfa1f726d48ae7f119ca0e73ca2856
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-10-09 12:43:48 +0530

    login register navbar done

--- product/src/App.js
+++ product/src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
-// import Usecases from './components/Usecases';
+import { Route, Routes} from 'react-router-dom';
+import Usecases from './components/Usecases';
 // import Another from './components/Another';
 import Onemore from './components/Onemore';
-// import Quiz from './components/quiz';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      {/* <Quiz /> */}
-      {/* <Another /> */}
-      <Onemore />
-      {/* <Usecases /> */}
+
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/quiz" element={<Onemore />} />
+          <Route path="/usecases" element={<Usecases />} />
+        </Routes>
+      </div>
+
     </div>
   );
 }
